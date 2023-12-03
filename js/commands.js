a4f7cedbf54bc54e0bb8f9d1b45171ca5c796066
Tsogoo1120 <95459702+Tsogoo1120@users.noreply.github.com>
2023-12-03 17:23:12 +0800

    initial commit

---
+++ js/commands.js
@@ -0,0 +1,74 @@
+var youtube = "https://www.youtube.com/fknight/";
+var twitter = "https://www.twitter.com/forrestpknight/";
+var password = "fkcodes";
+var linkedin = "https://www.linkedin.com/in/forrestpknight/";
+var instagram = "https://www.instagram.com/forrestpknight/";
+var github = "https://github.com/forrestknight/";
+var email = 'mailto:forrest@fkcodes.com';
+
+ readme = [
+    "<br>",
+    "Энэ бол Доор орших хот. Өдөр бүр энэ хотод гэмт хэрэг, хүчирхийлэл,.",
+    " аймшигтай бүхэн яг л энгийн зүйл мэт болж өнгөрдөг",
+    "Гэвч хотоос хүссэн бүхэн нь явж чаддаггүй. Доор орших хот-оос явахын тулд чамаас нийт",
+    " Чи энэ хотын бүх зүйлийг үзэн яддаг ба энэ хотоос явахыг хүсдэг. ",
+    " мөнгөн дүн шаардах бөгөөд энэ хотод ийм их хэмжээний мөнгийг ",
+    "бий болгох нь маш хэцүү. Нийт цуглуулах ёстой мөнгөн дүнг цуглуулж,",
+    "хотоос гарч чадвал чи ялж, тоглоом дуусна. Харин олж чадаагүй .",
+    "тохиолдолд чамайг зовлонт өдрүүд тосч байна",
+    "<br>"
+];
+
+
+instruction = [
+    "<br>",
+    " Таньд сонголтууд ирэх ба сонголтын урд харагдах тоог бичиж оруулсанаар таны сонголт баталгаажна  ",
+    "<br>"
+];
+
+
+secret = [
+    "<br>",
+    '<span class="command">sudo</span>           Only use if you\'re admin',
+    "<br>"
+];
+
+
+help = [
+    "<br>",
+    '<span class="command">readme</span>         Тоглоомын түүх',
+    '<span class="command">instruction</span>    Тоглох заавар',
+    '<span class="command">secret</span>         Нууц үг олох',
+    '<span class="command">history</span>        таны өмнө хийсэн сонголтуудын түүх гарч ирнэ',
+    '<span class="command">help</span>           энэ комманд юу хийдэгийг та мэдэж байгаа байх',
+    '<span class="command">clear</span>          цэвэрлэх',
+    '<span class="command">banner</span>         MSII зураг үзэх',
+    "<br>",
+];
+
+banner = [
+    '<span class="index">Raccoon team is Not A Corporation. All  Rights reserved.</span>',
+        "     ,---------------------------,",
+        "     |  /---------------------   |",
+        "     | |                      |  |",
+        "     | |                      |  |",
+        "     | |       Доор**с        |  |",
+        "     | |                      |  |",
+        "     | |                      |  |",
+        "     |  \_____________________/   |",
+        "     |___________________________|",
+        "   ,---\_____     []     _______/------,",
+        " /         /______________\           /|",
+        "/___________________________________ / | ___",
+        "|                                   |  |    )",
+        "|  _ _ _                 [-------]  |  |   (",
+        "|  o o o                 [-------]  |  /   _)_",
+        "|__________________________________ | /   /  /",
+        "/-------------------------------------/| ( )/",
+        "/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/ /v",
+        "/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/ /",
+
+        "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  Altantsog Gereltsetseg 2023",
+    '<span class="color2">Бидний бяцхан симуляц тоглоомонд тавтай морил.</span>',
+    "<span class=\"color2\">Тоглоомыг ажиллуулахын тулд  type</span> <span class=\"command\">'help'</span><span class=\"color2\">. <span class=\"color2\">гэж бичнэ үү</span></span>",
+];
